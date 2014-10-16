c1b53665348d98976771470920f4ac91d5832754
jaime7castro <jaime7castro@gmail.com>
2014-10-16 17:09:44 -0400

    Subida del Proyecto

---
+++ protected/views/site/index.php
@@ -0,0 +1,19 @@
+<?php
+/* @var $this SiteController */
+$this->pageTitle=Yii::app()->name;
+?>
+
+<center>
+    <h1>Bienvenido al <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+</center>
+
+
+<center>
+    <img src="images/guardia.jpg" alt="Guardia Boliviana"/>
+</center>
+
+<center>
+    <p>Para m&aacute;s informaci&oacute;n visite la p&aacute;gina de
+    <a href="http://www.armada.mil.bo/">Armada Boliviana</a>,
+    y adientrese a nuestro barco.</p>
+</center>
